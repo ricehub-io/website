28de5b1d55cb3f3623e483cd47f11c4f6bc10b8f
unknown <versioncontrol.undamaged050@passinbox.com>
2026-02-15 23:31:22 +0100

    remove fallback api url value

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,6 @@
 import { accessToken, addNotification } from "./appState";
 
-export const API_URL: string =
-    import.meta.env.VITE_API_URL || "http://127.0.0.1:3000";
+export const API_URL: string = import.meta.env.VITE_API_URL;
 
 type FetchMethod =
     | "GET"
