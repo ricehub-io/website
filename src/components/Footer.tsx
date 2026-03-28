5879cb90c483a2317e4acc3ca5f64db40b9b7b08
unaimeds <versioncontrol.undamaged050@passinbox.com>
2026-03-28 20:04:49 +0100

    update version

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ import DiscordIcon from "@/components/icons/DiscordIcon";
 import { ExternalLinkSchema } from "@/api/schemas";
 
 const VERSION_MAJOR: number = 1;
-const VERSION_MINOR: number = 17;
+const VERSION_MINOR: number = 18;
 
 export default function Footer() {
     const discord = useSignal("");
